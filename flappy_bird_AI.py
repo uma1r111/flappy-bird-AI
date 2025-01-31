25d596b03f636f62227270dcc52c049ada262de5
Umair <shaikhumair297@gmail.com>
2025-01-31 23:02:53 +0500

    final changes

--- flappy_bird_AI.py
+++ flappy_bird_AI.py
@@ -10,6 +10,7 @@ pygame.init()
 
 WINDOWS_WIDTH=500
 WINDOWS_HEIGHT=800
+GEN=0
 
 # 2x for scaling and visibility
 BIRD_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), 
@@ -165,7 +166,7 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-def draw_window(win, bird, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0,0))
 
     for pipe in pipes:
@@ -174,21 +175,26 @@ def draw_window(win, bird, pipes, base, score):
     text=STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
     win.blit(text, (WINDOWS_WIDTH - 10 - text.get_width(), 10))
 
-    base.draw(win)
+    text=STAT_FONT.render("Gen: " + str(gen), 1, (255, 255, 255))
+    win.blit(text, (10,10))
 
-    bird.draw(win)
+    base.draw(win)
+    for bird in birds:
+        bird.draw(win)
     pygame.display.update()
 
 # since all the data is here, this main func will become our fitness function as well which is being passed in the winner func    
 def main(genomes, config):
     # to keep track of neural networks for birds, each position(nets, ge, birds) will correspond to same bird
+    global GEN
+    GEN += 1
     nets=[]
     ge=[]
     birds=[]
 
     # creating neural network
-    for g in genomes:
-        net=neat.nn.FeedForwardNetwork(g, config)
+    for _, g in genomes:
+        net=neat.nn.FeedForwardNetwork.create(g, config)
         nets.append(net)
         birds.append(Bird(230, 350))
         ge.append(g)
@@ -209,8 +215,29 @@ def main(genomes, config):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
+                pygame.quit()
+                quit()
         
-        #bird.move()
+        # if bird passes first pipe then convert it to second pipe
+        pipe_ind = 0
+        if len(birds)>0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+            else:
+                run=False 
+                break # no birds left so quit game
+        
+        # to not allow bird to fly all the way up or down, just enough to make it survive
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1        
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+           
+            # output neuron is list
+            if output[0]>0.5:
+                bird.jump()
+
         add_pipe=False
         remove=[]
         for pipe in pipes:
@@ -242,19 +269,13 @@ def main(genomes, config):
             pipes.remove(r)
 
         for x, bird in enumerate(birds):
-             if bird.y + bird.img.get_height() >= 730:
+             if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
                 birds.pop(x)
                 nets.pop(x)
                 ge.pop(x)
 
         base.move()
-        draw_window(win, bird, pipes, base, score)
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+        draw_window(win, birds, pipes, base, score, GEN)
 
 def run(config_path): # define all the different sub-headings we used in config file
     config=neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
